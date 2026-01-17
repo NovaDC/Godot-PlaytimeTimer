3dff315dd117c59f0f1640562ed5af573cc42cb9
NovaDC <164206531+NovaDC@users.noreply.github.com>
2026-01-17 00:58:34 -0600

    Assist script documentation loading
    Makes the scripts automatically load in the editor when appropriate, to make the editor documentation viewer more reliable for those scripts.

--- addons/playtime_timer/plugin.gd
+++ addons/playtime_timer/plugin.gd
@@ -8,12 +8,27 @@ const PLUGIN_ICON:Texture2D = preload("./icon.svg")
 const AUTOLOAD_NAME := "PlaytimeManager"
 const AUTOLOAD_SCRIPT := "./playtime_manager.gd"
 
+const ENSURE_SCRIPT_DOCS:Array[Script] = [
+	preload("./playtime_timer.gd"),
+	preload("./playtime_manager.gd"),
+]
+
+# Every once ands a while the script docs simply refuse to update properly.
+# This nudges the docs into a ensuring that the important scripts added by
+# this addon are actually loaded.
+func _ensure_script_docs() -> void:
+	var edit := get_editor_interface().get_script_editor()
+	for scr in ENSURE_SCRIPT_DOCS:
+		edit.update_docs_from_script(scr)
+
 func _enter_tree():
+	_ensure_script_docs()
 	if EditorInterface.is_plugin_enabled(PLUGIN_NAME_INTERNAL):
 		if not Engine.has_singleton(AUTOLOAD_NAME):
 			add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_SCRIPT)
 
 func _enable_plugin():
+	_ensure_script_docs()
 	if not Engine.has_singleton(AUTOLOAD_NAME):
 		add_autoload_singleton(AUTOLOAD_NAME, AUTOLOAD_SCRIPT)
 
